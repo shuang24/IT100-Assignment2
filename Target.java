20ea84c290ea744934a480862f05e09300b85c69
S <s>
2015-04-26 14:24:35 -0700

    {"type":"TI", "runs":"1"}

--- Target.java
+++ Target.java
@@ -11,7 +11,19 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+	private static final double pixels_per_sq_inch = 72;
+	private static final double circle_one_radius = 1*pixels_per_sq_inch;
+	private static final double circle_two_radius = 0.3*pixels_per_sq_inch;
+	private static final Color color_one = Color.RED;
+	private static final color color_two = Color.WHITE;
+	
 	public void run() {
-		/* You fill this in. */
+		// Add 3 circle to make a target
+		
+		add(centredCircle(circle_one_radius, color_one));
+		
+	}
+	private GOval centredCircle (double r, Color color) {
+		
 	}
 }
